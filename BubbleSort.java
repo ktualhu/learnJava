17b4a041494174d0258a55dbbbb13436e7889cb7
ktualhu <ktulhu0201@gmail.com>
2017-11-15 09:50:33 +0300

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class BubbleSort {
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         Random rand = new Random();
         int a[] = new int[10];
         int t = 0;
@@ -29,5 +29,5 @@ public class BubbleSort {
         for(int i = 0; i < a.length; i++) {
             System.out.println("a[" + i + "] = " + a[i]);
         }
-    }*/
+    }
 }
